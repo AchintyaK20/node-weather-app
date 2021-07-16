0100e314b3a4ca13e19bb61a7260782d62c06556
Achintya Kumar <56220374+AchintyaK20@users.noreply.github.com>
2021-07-16 11:57:30 +0530

    Logo change

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'Contact achintyakv10a1@gmail.com for help.',
         title: 'Help',
         name: 'Achintya Kumar'
     })
